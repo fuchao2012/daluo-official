7b5d509cb7b4eec9581309a0b6bfcf514b24f010
郑付超 <hzzhengfuchao01@corp.netease.com>
2017-08-12 11:57:55 +0800

    ADD: 完成气象风险

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./weather.component.css']
 })
 export class WeatherComponent implements OnInit {
-
+  weatherPics = []
   constructor() { }
 
   ngOnInit() {
+    this.weatherPics = [
+      { src: "../../assets/images/weather1.png", alt: "能源", active: true },
+      { src: "../../assets/images/weather2.png", alt: "农业", active: false },
+      { src: "../../assets/images/weather3.png", alt: "交通", active: false },
+      { src: "../../assets/images/weather4.png", alt: "旅游", active: false },
+    ]
   }
-
+  active(img){
+    this.weatherPics.filter((image)=>image.alt===img.alt ? image.active = true :image.active = false );
+  }
 }
